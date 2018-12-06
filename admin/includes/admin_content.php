6e7b4ba26e23b4d2c41a7addafedd1d2e436f297
anmihovil <anmihovil@gmail.com>
2018-12-06 20:53:22 +0100

    Commit after lesson 91

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,96 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Admin
+                <small>Subheading</small>
+            </h1>
+
+            <?php
+
+            // $user = new User();
+            // $user->username="bakmihovil";
+            // $user->save();
+
+            // $user = User::find_user_by_id(4);
+            // $user->username="ivanoreb";
+            //
+            // $user->save();
+            //$user->delete();
+
+
+            // $user->last_name="Ickx";
+            // $user->update();
+
+            // Following lines of code are using create() method to make a new user
+            // $user = new User();
+            // $user->username = "2ndExample_username";
+            // $user->password = "2ndExample_password";
+            // $user->first_name = "Jacky";
+            // $user->last_name = "Stewart";
+            //
+            // $user->create();
+
+
+            /*
+            if($database->connection){
+              echo "Database connection established!"."<br>";
+            }
+            */
+
+            /*
+            This was part of first solution without User class
+            $sql = "SELECT * FROM users WHERE _id=1";
+            $result = $database->query($sql);
+            $user_found = mysqli_fetch_array($result);
+            echo $user_found['username'];
+            */
+
+            // After making method static no need to instantiate the class User
+            //$user = new User();
+
+            // $users = User::find_all_users();
+            // foreach ($users as $user) {
+            //   // code...
+            //   echo $user->username."<br>";
+            // }
+
+            // while($row = mysqli_fetch_array($result_set)){
+            //   echo $row['username']."<br>";
+            // }
+
+            //New function used this one commented out
+            // $found_user = User::find_user_by_id(2);
+            // echo $found_user->username;
+
+            // $user = User::instantiation($found_user);
+
+            // $user = new User();
+            //
+            // $user->_id = $found_user['_id'];
+            // $user->username = $found_user['username'];
+            // $user->password = $found_user['password'];
+            // $user->first_name = $found_user['first_name'];
+            // $user->last_name = $found_user['last_name'];
+
+            //echo $found_user['username'];
+            // echo $user->last_name;
+
+            ?>
+
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
---
+++ admin/includes/user.php
@@ -0,0 +1,159 @@
+<?php
+
+class User{
+
+        public $_id;
+        public $username;
+        public $password;
+        public $first_name;
+        public $last_name;
+        protected static $db_table = "users"; //A property to replace 'users' anywhere in the code
+        protected static $db_table_fields =array('username','password','first_name','last_name');
+
+        public static function find_all_users(){
+        // global $database;
+        // $result_set = $database->query("SELECT * FROM users");
+        // return $result_set;
+        return self::find_this_query("SELECT * FROM users");
+      }
+
+      public static function find_user_by_id($user_id){
+        global $database;
+        $the_result_array = self::find_this_query("SELECT*FROM users WHERE _id=$user_id LIMIT 1");
+
+        //This single line subsitutes all commented code afterwards - ternary operator
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+        //$found_user = mysqli_fetch_array($result_set);
+        // if(!empty($the_result_array)){
+        //   $first_item = array_shift("$the_result_array");
+        //   return $first_item;
+        // }else{
+        //   return false;
+        // }
+        //Next line corrected by Edwin from the future
+        //return $found_user;
+      }
+
+      public static function find_this_query($sql){
+        global $database;
+        $result_set = $database->query($sql);
+        $the_object_array = array();
+
+        while($row = mysqli_fetch_array($result_set)){
+          $the_object_array[] = self::instantiation($row);
+        }
+        return $the_object_array;
+      }
+
+      public static function verify_user($username, $password){
+        global $database;
+        $username = $database->escape_string($username);
+        $password = $database->escape_string($password);
+
+        $sql = "SELECT * FROM users WHERE ";
+        $sql.= "username = '{$username}' ";
+        $sql.= "AND password = '{$password}' ";
+        $sql.= "LIMIT 1 ";
+
+        $the_result_array = self::find_this_query($sql);
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
+      }
+
+
+      public static function instantiation($the_record){
+        $the_object = new self;
+
+        // $the_object->_id        = $found_user['_id'];
+        // $the_object->username   = $found_user['username'];
+        // $the_object->password   = $found_user['password'];
+        // $the_object->first_name = $found_user['first_name'];
+        // $the_object->last_name  = $found_user['last_name'];
+
+        foreach ($the_record as $the_attribute => $value) {
+          // code...
+          if($the_object->has_the_attribute($the_attribute)){
+            $the_object->$the_attribute = $value;
+          }
+        }
+        return $the_object;
+      }
+
+      private function has_the_attribute($the_attribute){
+        $object_properties = get_object_vars($this);
+        return array_key_exists($the_attribute, $object_properties);
+
+      }
+
+      protected function properties(){
+        //return get_object_vars($this);
+        $properties = array();
+        foreach ($self::$db_table_fields as $db_field) {
+          // code...
+          if(property_exists($this, $db_field)){
+            //using dynamic property ad hoc
+            $properties[$db_field]=$this->$db_field;
+          }
+        }
+        return $properties;
+      }
+
+      public function save(){
+        global $database;
+        return isset($this->_id) ? $this->update() : $this->create();
+      }
+
+      public function create(){
+        global $database;
+
+        $properties = $this->properties();
+
+        //$sql = "INSERT INTO ".self::$db_table ." (username,password,first_name,last_name)";
+        // $sql.= "VALUES ('";
+        // $sql.= $database->escape_string($this->username). "',' ";
+        // $sql.= $database->escape_string($this->password). "',' ";
+        // $sql.= $database->escape_string($this->first_name). "','";
+        // $sql.= $database->escape_string($this->last_name). "')";
+
+        //Properties abstraction routine - improved solution
+        $sql = "INSERT INTO ".self::$db_table ."(". implode(",", array_keys($properties)) .")";
+        $sql.= "VALUES ('".implode("','", array_values($properties))."')";
+
+        if($database->query($sql)){
+          $this->id = $database->the_insert_id();
+          return true;
+        }else{
+          return false;
+        }
+      }
+
+      public function update(){
+        global $database;
+        $sql = "UPDATE users SET ";
+        $sql.= "username='" . $database->escape_string($this->username)     . "', ";
+        $sql.= "password='" . $database->escape_string($this->password)     . "', ";
+        $sql.= "first_name='" . $database->escape_string($this->first_name) . "', ";
+        $sql.= "last_name='" . $database->escape_string($this->last_name)   . "' ";
+        $sql.= " WHERE _id= " . $database->escape_string($this->_id);
+
+        $database->query($sql);
+
+        return (mysqli_affected_rows($database->connection)==1) ? true : false;
+      }
+
+      public function delete(){
+        global $database;
+        $sql = "DELETE FROM users ";
+        $sql.= "WHERE _id=".$database->escape_string($this->_id);
+        $sql.= " LIMIT 1";
+
+        $database->query($sql);
+
+        return (mysqli_affected_rows($database->connection)==1) ? true : false;
+      }
+
+} //End of class User
+
+
+
+?>
